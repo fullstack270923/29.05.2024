bf30e06863952e329b6341c41a139bf1f12a70db
fullstack290623 <fullstack290623@gmail.com>
2024-05-29 20:35:10 +0300

    .

--- proj3/src/com/company/Main.java
+++ proj3/src/com/company/Main.java
@@ -1,5 +1,12 @@
 package com.company;
 
+import jdk.swing.interop.SwingInterOpUtils;
+import org.w3c.dom.ls.LSOutput;
+
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter; // require
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.StringTokenizer;
 
@@ -46,12 +53,63 @@ public class Main {
         System.out.println(full_name.substring(0, full_name.indexOf(".")));
         System.out.println(full_name.substring(full_name.indexOf(".") + 1));
 
-        //String full_name = "john.Kennedy";
+        //String full_name = "john.Kennedy.123.456.789";
         StringTokenizer tokenizer = new StringTokenizer(full_name, ".");
         while (tokenizer.hasMoreElements()) {
             System.out.println(tokenizer.nextToken());
         }
 
+        System.out.println(full_name.replace("john", ""));
+
+        System.out.println((String)LocalDateTime.now().toString());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        System.out.println(LocalDateTime.now().format(formatter));
+
+        int[] numbers = new int[10];
+        System.out.println(numbers[9]);
+        numbers[5] = 5;
+        System.out.println(numbers); // [I@5bcab519
+        int x = 5;
+        //var y = 5; // changes the var to the most correct type -- not recommended in these cases
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(numbers));
+
+        String cars[] = new String[3];
+        cars[0] = "Mitsubishi";
+        cars[1] = "Honda";
+        cars[2] = "Fiat";
+        String cars2[] = {"Mitsubishi", "Honda", "Fiat"};
+        System.out.println(Arrays.toString((cars2)));
+        System.out.println();
+        System.out.println("{\"Mitsubishi\", \"Honda\", \"Fiat\"} length = " + new String[] {"Mitsubishi", "Honda", "Fiat"}.length);
+        System.out.println(Arrays.toString(new int[] { 1, 4, 7}));
+
+        int[][] seats = new int[2][];
+        seats[0] = new int[] { 1, 2, 4};
+        seats[1] = new int[] { 1 };
+
+        int[][] seats2 = {
+                {1, 2, 4},
+                {1}
+        };
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print((i + 1) + " ");
+        }
+        System.out.println();
+
+        for(int number: new int[] {1,2,3}) {
+
+        }
+        Arrays.stream((new int[]{1, 2, 3})).forEach(y -> System.out.print(y));
+        System.out.println();
+        Arrays.stream(new int[]{0, 1, 2, 3, 4}).filter(y -> y > 2).forEach(y -> System.out.print(y));
+        System.out.println();
+        Arrays.stream(new int[]{0, 1, 2, 3, 4, 5}).map(y -> y * y).forEach(y -> System.out.print(y + " "));
         System.out.println();
     }
 }
